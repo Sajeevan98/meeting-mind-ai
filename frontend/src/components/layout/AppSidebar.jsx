c57604eb13ca485f84579a3c785cd04b31b783ab
Sajeevan98 <sajee91098@gmail.com>
2026-08-04 00:19:30 +0530

    refactor(frontend): clean up meeting components and utilities

--- frontend/src/components/layout/AppSidebar.jsx
+++ frontend/src/components/layout/AppSidebar.jsx
@@ -1,39 +1,56 @@
-import { Drawer, Toolbar, List, ListItemButton, ListItemText } from "@mui/material";
+import { Drawer, Toolbar, List, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { DRAWER_WIDTH } from "./layoutConstants";
 import { NavLink } from "react-router-dom";
 
 export default function AppSidebar({ mobileOpen, onDrawerToggle }) {
 
+    const navItemSx = {
+        mx: 1,
+        borderRadius: 1,
+        color: "text.primary",
+
+        "&.active": {
+            bgcolor: "primary.main",
+            color: "primary.contrastText",
+        },
+
+        "&.active:hover": {
+            bgcolor: "primary.dark",
+        },
+    };
+
     const drawer = (
-
         <>
-            <Toolbar />
+            
+            <Typography color="primary" sx={{fontSize: 24, fontWeight: 600, marginX: 'auto', marginY: 3}} >MeetingMind AI</Typography>
+            {/* <Toolbar /> */}
             <List>
-
-                <ListItemButton 
-                    onClick={onDrawerToggle}
+                <ListItemButton
                     component={NavLink}
                     to="/"
+                    end
+                    onClick={onDrawerToggle}
+                    sx={navItemSx}
                 >
                     <ListItemText primary="Dashboard" />
                 </ListItemButton>
 
                 <ListItemButton
-                    onClick={onDrawerToggle}
                     component={NavLink}
                     to="/meetings"
+                    onClick={onDrawerToggle}
+                    sx={navItemSx}
                 >
                     <ListItemText primary="Meetings" />
                 </ListItemButton>
 
                 <ListItemButton onClick={onDrawerToggle}>
-                    <ListItemText primary="Analysis" />
+                    <ListItemText primary="Features" />
                 </ListItemButton>
 
                 <ListItemButton onClick={onDrawerToggle}>
                     <ListItemText primary="Settings" />
                 </ListItemButton>
-
             </List>
         </>
     );
